c966af8139e08aa668bbf8ad3ddb8a4b8e9ed61b
T8KNaps <jordan.l.lindsey@gmail.com>
2025-03-14 09:05:51 -0400

    Add error capture for missing marker

--- src/730_dash/scrape.py
+++ src/730_dash/scrape.py
@@ -3,19 +3,21 @@ This module defines the classes needed to process a 730DC newsletter url
 """
 
 import requests
+from uuid import uuid4
 
 from bs4 import BeautifulSoup, Tag
 
 
 class Blurb:
     def __init__(self, order: int = 0, title: str = "", body: str = "", b_type: str = ""):
+        self.id = uuid4()
         self.order = order
         self.title = title
         self.body = body
         self.b_type = b_type
 
     def __str__(self):
-        return (f"Blurb (Order: {self.order}, Title: '{self.title}', Type: {self.b_type})")
+        return (f"Blurb (ID: {self.id}, Type: {self.b_type}, Title: '{self.title}')")
 
 
 class NlHandler:
@@ -42,17 +44,54 @@ class NlHandler:
         """
         Create the markers (bs4.Tag objects) used to triangulate a Blurb.type. 
         """
+
+        marker_dict = {}
+
+        img_url = [
+            # what to know
+            ("wtk", "https://gallery.mailchimp.com/576dfd24a3c9e732d2920f811/images/99c7afbf-22e1-485a-befa-77bfa4c09cc1.png"),
+            # what to do
+            ("wtd", "https://gallery.mailchimp.com/576dfd24a3c9e732d2920f811/images/686d06ad-1691-48c9-af9d-ec5766c203e3.png"),
+            # weekly scheduler
+            ("ws", "https://gallery.mailchimp.com/576dfd24a3c9e732d2920f811/images/6e6f9b72-2610-49ae-9332-3ec026982100.png")
+        ]
+
+        for img in img_url:
+            try:
+                marker = self.content.find_all(name="img", src=img[1])
+                marker = marker[0]
+            
+            # catch when the marker isn't in the newsletter
+            except IndexError:
+                print(f"Error: {img[0].upper()} image marker not found for {self.url}")
+                continue
+            
+            if img[0] == "wtk":
+                marker_dict["wtk"] = marker
+            else:
+                marker_dict["wtd"] = marker
+            
+        return marker_dict
         
+        try:
+            # what to know
+            wtk_marker = self.content.find_all(name="img", 
+                                    src="https://gallery.mailchimp.com/576dfd24a3c9e732d2920f811/images/99c7afbf-22e1-485a-befa-77bfa4c09cc1.png")
+            wtk_marker = wtk_marker[0]
 
-        # what to know
-        wtk_marker = self.content.find_all(name="img", 
-                                src="https://gallery.mailchimp.com/576dfd24a3c9e732d2920f811/images/99c7afbf-22e1-485a-befa-77bfa4c09cc1.png")
-        wtk_marker = wtk_marker[0]
+            # what to do
+            wtd_marker = self.content.find_all(name="img", 
+                                    src="https://gallery.mailchimp.com/576dfd24a3c9e732d2920f811/images/686d06ad-1691-48c9-af9d-ec5766c203e3.png")
+            wtd_marker = wtd_marker[0]
 
-        # what to do
-        wtd_marker = self.content.find_all(name="img", 
-                                src="https://gallery.mailchimp.com/576dfd24a3c9e732d2920f811/images/686d06ad-1691-48c9-af9d-ec5766c203e3.png")
-        wtd_marker = wtd_marker[0]
+            # # weekly scheduler
+            # wtd_marker = self.content.find_all(name="img", 
+            #                         src="https://gallery.mailchimp.com/576dfd24a3c9e732d2920f811/images/686d06ad-1691-48c9-af9d-ec5766c203e3.png")
+            # wtd_marker = wtd_marker[0]
+
+        except IndexError:
+            print(f"Error: Marker for '{self.url}' not found.")
+        
 
         return {
             "wtk": wtk_marker,
@@ -116,7 +155,17 @@ class NlHandler:
 
 if __name__ == "__main__":
     # testing
-    url = "https://us7.campaign-archive.com/?u=576dfd24a3c9e732d2920f811&id=c35f0dd0aa"
-    # url = "https://us7.campaign-archive.com/?u=576dfd24a3c9e732d2920f811&id=ec7673a6f4"
-    handler = NlHandler(url=url)
-    handler._breakout_blurbs()
+    url_list = [
+        # "https://us7.campaign-archive.com/?u=576dfd24a3c9e732d2920f811&id=c35f0dd0aa",
+        # "https://us7.campaign-archive.com/?u=576dfd24a3c9e732d2920f811&id=ec7673a6f4",
+        # For url below, see if the font size mistake makes a difference
+        # "https://us7.campaign-archive.com/?u=576dfd24a3c9e732d2920f811&id=63b262de00",
+        # For the url below, need to add a 'weekly scheduler' img marker for when there is no wtd marker
+        "https://us7.campaign-archive.com/?u=576dfd24a3c9e732d2920f811&id=04ac2c19ea"
+    ]
+
+
+    for url in url_list:
+        handler = NlHandler(url=url)
+        handler._breakout_blurbs()
+        # print("-------------------------------------------------")
